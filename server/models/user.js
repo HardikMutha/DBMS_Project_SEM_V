138fa3fb5d4ef822721be5e1546299a37e66b632
HardikMutha <hardikmutha2005@gmail.com>
2025-10-11 10:35:04 +0530

    Feat : Auth Setup Complete,Needs more testing though

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,20 @@
 import pool from "../db/config.js";
 
 export const createUser = async function ({ username, email, password }) {
-  const [result] = await pool.query(
-    `INSERT INTO Users (username, email, role, password) VALUES (?, ?, 'user', ?)`,
-    [username, email, password]
-  );
-  console.log(result);
+  const [result] = await pool.query(`INSERT INTO Users (username, email, role, password) VALUES (?, ?, 'user', ?)`, [
+    username,
+    email,
+    password,
+  ]);
+  return result;
+};
+
+export const getUserByUsernameOrEmail = async function (identifier) {
+  const [rows] = await pool.query(`SELECT * FROM Users WHERE username = ? OR email = ?`, [identifier, identifier]);
+  return rows[0];
+};
+
+export const getUserById = async function (id) {
+  const [rows] = await pool.query(`SELECT * FROM Users WHERE id = ?`, [id]);
+  return rows[0];
 };
